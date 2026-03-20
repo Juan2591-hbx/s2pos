c772a5097e1843abafa86c71983e2b72966703af
Juan2591-hbx <amartinez@herbax.com.mx>
2026-03-19 20:08:19 -0600

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    appDir: false, // Desactiva App Router para usar Pages Router
+  },
 };
 
 export default nextConfig;
